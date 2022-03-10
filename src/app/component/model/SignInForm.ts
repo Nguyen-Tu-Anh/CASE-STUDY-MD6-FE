0553d12bebfa11bf0ca490879d219783b1c7f5a1
binkonchuot <nguyenducthang231295@gmail.com>
2022-03-10 13:58:10 +0700

    login giao dien vao

---
+++ src/app/component/model/SignInForm.ts
@@ -0,0 +1,12 @@
+// KIEU DU LIEU JSON
+
+export class SignInForm{
+  private username!: string;
+  private password!: string;
+
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+}
